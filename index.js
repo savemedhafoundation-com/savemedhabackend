c729a2ff9cdc68fa807480013a5ac164ff5a011b
kausik <kkausik11@g,ail.com>
2025-11-27 13:31:20 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+module.exports = app;
